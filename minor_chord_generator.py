a4c53f4a60f72f5fd37ecee54434111848f09dae
deepakjami <87440240+deepakjami@users.noreply.github.com>
2024-04-15 19:30:15 +0530

    Update minor_chord_generator.py

--- minor_chord_generator.py
+++ minor_chord_generator.py
@@ -1,10 +1,8 @@
 def generate_minor_chords(root_note,maximum):
     minor_chords = []
-    # Calculate the minor third and perfect fifth relative to the root note
     root_note -= 12
     minor_third = root_note + 3
     perfect_fifth = root_note + 7
-    # Construct the minor chord using the root note, minor third, and perfect fifth
     f = True
     if root_note + 12 <= maximum:
         f = False
@@ -14,8 +12,6 @@ def generate_minor_chords(root_note,maximum):
         f = False
     if f :
         perfect_fifth -= 12
-
-    minor_chord = [root_note -12,root_note + 12, minor_third + 12, perfect_fifth + 12]
-    # Append the minor chord to the list of minor chords
+    minor_chord = [root_note -12,root_note + 12, minor_third + 12, perfect_fifth + 12
     minor_chords.append(minor_chord)
     return minor_chords
